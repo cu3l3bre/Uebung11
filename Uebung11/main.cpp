1c11aa7c347a85ee9a9e7c6c967fbcd526f2e80d
cu3l3bre <cu3l3bre@googlemail.com>
2019-05-17 08:51:57 +0200

    added region for Aufgabe1

--- Uebung11/main.cpp
+++ Uebung11/main.cpp
@@ -8,6 +8,12 @@ int main()
 	cout << "Programm zu Uebung 11" << endl << endl;
 
 
+#pragma region Aufgabe1 
+
+	/*
+	
+	// auskommentiert um sich auf Aufgabe 2 zu konzentrieren
+
 	// TODO Arrays mit Allgemeinen-Konstruktoren? -> OKAY scheinbar ist das nicht so einfach möglich, da für array der standard konstruktor benötigt wird
 	Kunde kunde1(123,"Max","Mustermann","Musterstrasse 5a", a);
 	Kunde kunde2(456, "Maxine", "Musterfrau", "Baumstrasse", w);
@@ -16,16 +22,12 @@ int main()
 	//cout << "ByteGroesse von kunde1: " << sizeof(kunde1) << endl;
 
 
-	// Array von Klasse Kunde mit 100 Elementen
-	Kunde kunden[100];
-
-
 	{
 		Kunde destruktortest; // Das Objekt ist nur in diesem Bock gültig , deswegen kommt die Meldung auch das der Destruktor aufgerufen wurde
 	}
 
 
-	/*
+	
 	
 	kunde1.zeigeKundendaten();
 	kunde2.zeigeKundendaten();
@@ -35,9 +37,17 @@ int main()
 	//kunde1.kundendatenEingeben();
 
 	*/
+	
+#pragma endregion
+
 
 
 	// mehrere Kunden anlegen
+	
+	// Array von Klasse Kunde mit 100 Elementen
+	Kunde kunden[100];
+
+
 	char eingabeWeitermachen = 'j';
 	int anzahlAngelegterKunden = 0;
 
@@ -53,8 +63,6 @@ int main()
 		cin >> eingabeWeitermachen;
 		system("cls");
 	}
-
-
 
 	cout << "Es sind folgende Kunden angelegt" << endl;
 	cout << endl;
